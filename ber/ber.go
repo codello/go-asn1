741ff08d1dc37c6b706bf8f80751dfec97f76152
Kim Wittenburg <kim@codello.dev>
2025-02-23 17:44:25 +0100

    Update package documentation

--- ber/ber.go
+++ ber/ber.go
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package ber implements the ASN.1 Basic Encoding Rules as defined in [Rec.
-// ITU-T X.690]. See also “[A Layman's Guide to a Subset of ASN.1, BER, and
-// DER]”.
+// Package ber implements the ASN.1 Basic Encoding Rules (BER). The Basic
+// Encoding Rules are defined in [Rec. ITU-T X.690].
+// See also “[A Layman's Guide to a Subset of ASN.1, BER, and DER]”.
 //
 // See the package documentation of the asn1 package for details how Go types
 // translate to ASN.1 types. Types following that specification can be encoded
@@ -14,6 +14,9 @@
 //   - When decoding an ASN.1 INTEGER type into a Go integer the size of the
 //     integer is limited by the size of the Go type. This limitation does not apply
 //     to [*math/big.Int].
+//   - When decoding an ASN.1 REAL type into a Go float64 or float32, the size of
+//     the value is limited by the size of the Go type. When using [*math/big.Float]
+//     the size limitations of that type apply.
 //   - When decoding binary data into a pre-allocated byte slice the data will
 //     overwrite existing data in the slice.
 //   - When decoding binary data into a byte array, the number of bytes in the
